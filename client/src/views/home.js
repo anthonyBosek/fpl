cf52ff2274e5af698bea190bbce1d229bb91c4f6
Anthony Bosek <anthony.bosek@gmail.com>
2023-12-16 19:45:05 -0700

    home hero

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -2,8 +2,8 @@ import "../styles/home.css";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
-      <h2>Welcome page shows general league data, news, table</h2>
+      <div className="hero"></div>
+      <div className="home-title">Welcome to the Football App</div>
     </div>
   );
 };
